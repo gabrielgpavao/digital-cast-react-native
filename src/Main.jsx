7e3dea9fd29d1b7cb8fb36c3a3c69967d696de3d
Gabriel <gabpavao@outlook.com>
2023-03-21 17:02:50 -0300

    create Main component and its style

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyledMainBackground } from './StyledMainBackground';
+
+export function Main () {
+    return (
+        <>
+            <StatusBar/>
+            <StyledMainBackground>
+            </StyledMainBackground>
+        </>
+    )
+}
