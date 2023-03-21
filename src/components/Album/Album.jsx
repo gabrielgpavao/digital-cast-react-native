d9e9609365ac996538447dd4509dd31653f44eb0
Gabriel <gabpavao@outlook.com>
2023-03-21 17:15:05 -0300

    feat: create Album component and its style

---
+++ src/components/Album/Album.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledAlbum, StyledAlbumImage, StyledTitle } from './StyledAlbum'
+
+export function Album () {
+	return (
+		<StyledAlbum>
+			<StyledTitle>Selecione um áudio</StyledTitle>
+			<StyledAlbumImage source={{}}/>
+		</StyledAlbum>
+	)
+}
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Album } from '../../components/Album/Album';
 import { Header } from '../../components/Header/Header';
 
 export function Player () {
@@ -7,6 +8,7 @@ export function Player () {
 		<SafeAreaProvider>
 			<SafeAreaView>
                 <Header/>
+				<Album/>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	)
