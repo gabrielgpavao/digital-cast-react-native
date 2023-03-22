ef35b0c78e697b5e36d7fb7d37522a2860e2eaf0
Gabriel <gabpavao@outlook.com>
2023-03-22 11:42:05 -0300

    feat: switch to the next or previous audio

--- src/components/AudioControllers/AudioControllers.jsx
+++ src/components/AudioControllers/AudioControllers.jsx
@@ -8,12 +8,36 @@ import { Feather } from '@expo/vector-icons';
 import { useAudioContext } from '../../hooks/useAudioContext';
 
 export function AudioControllers() {
-	const { isPlaying, toggleAudio } = useAudioContext()
+	const { playlist, isPlaying, toggleAudio, currentAudioInfo, playSong } = useAudioContext()
+
+	function playNextAudio () {
+        let nextAudio = playlist.find((audio) => {
+            return Number(audio.id) === (Number(currentAudioInfo.id) + 1)
+        })
+        
+        if (!nextAudio) {
+            nextAudio = playlist.find((audio) => audio.id ===  '1')
+        }
+
+        playSong(nextAudio, true)
+    }
+
+    function playPrevAudio () {
+        let nextAudio = playlist.find((audio) => {
+            return Number(audio.id) === (Number(currentAudioInfo.id) - 1)
+        })
+        
+        if (!nextAudio) {
+            nextAudio = playlist.find((audio) => audio.id ===  playlist[playlist.length - 1].id)
+        }
+
+        playSong(nextAudio, true)
+    }
 	
 	return (
 		<StyledAudioController>
 			<StyledButton>
-				<StyledControllers>
+				<StyledControllers onPress={playPrevAudio}>
 					<Feather name='chevron-left' size={24} color='#ffffff' />
 				</StyledControllers>
 
@@ -21,7 +45,7 @@ export function AudioControllers() {
 					<Feather name={ isPlaying ? 'pause' : 'play'} size={32} color='#ffffff' />
 				</StyledControllers>
 
-				<StyledControllers>
+				<StyledControllers onPress={playNextAudio}>
 					<Feather name='chevron-right' size={24} color='#ffffff' />
 				</StyledControllers>
 			</StyledButton>
