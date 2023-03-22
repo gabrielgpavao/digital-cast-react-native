10edb4af880cacf0f52cc66c0845a5aea8ea4320
Gabriel <gabpavao@outlook.com>
2023-03-22 08:34:25 -0300

    feat: create functions and states to play or pause a song

--- src/context/AudioContext.jsx
+++ src/context/AudioContext.jsx
@@ -1,12 +1,45 @@
 import React, { createContext, useState } from 'react';
+import { Audio } from 'expo-av';
 
 export const AudioContext = createContext({})
 
 export function AudioProvider ({ children }) {
     const [playlist, setPlaylist] = useState([])
+    const [currentAudio, setCurrentAudio] = useState()
+    const [currentAudioInfo, setCurrentAudioInfo] = useState()
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    async function playSong (source, autoPlay = false) {
+        if (currentAudio) {
+            await currentAudio.unloadAsync()
+        }
+        
+        setCurrentAudioInfo(source)
+
+        const { audio } = Audio.Sound.createAsync({
+            uri: source.uri
+        })
+
+        setCurrentAudio(audio)
+
+        if (autoPlay) {
+            setIsPlaying(true)
+            await audio.playAsync()
+        }
+    }
+
+    async function toggleAudio () {
+        if (isPlaying) {
+            await currentAudio.pauseAsync()
+        } else {
+            await currentAudio.playAsync()
+        }
+
+        setIsPlaying((prev) => !prev)
+    }
     
     return (
-        <AudioContext.Provider value={{ playlist, setPlaylist }}>
+        <AudioContext.Provider value={{ playlist, setPlaylist, currentAudioInfo, playSong, toggleAudio }}>
             {children}
         </AudioContext.Provider>
     )
