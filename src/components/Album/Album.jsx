974b72e637b50152ae009d47cfc8f8b46a29d257
Gabriel <gabpavao@outlook.com>
2023-03-22 08:52:51 -0300

    feat: render song name and its album dinamically

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { StyledAlbum, StyledAlbumImage, StyledTitle } from './StyledAlbum'
+import { useAudioContext } from '../../hooks/useAudioContext'
 
 export function Album () {
+	const { currentAudioInfo } = useAudioContext()
+	
 	return (
 		<StyledAlbum>
-			<StyledTitle>Selecione um áudio</StyledTitle>
-			<StyledAlbumImage source={{ uri: 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg' }}/>
+			<StyledTitle>{ currentAudioInfo?.title || 'Selecione um Áudio'}</StyledTitle>
+			<StyledAlbumImage
+				source={{
+					uri: currentAudioInfo?.imageSource || 'https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg'
+				}}
+			/>
 		</StyledAlbum>
 	)
 }
